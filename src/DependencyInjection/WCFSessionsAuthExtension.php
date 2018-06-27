71da82ce46a578f4db89606f93ebfc28cbe78d7c
xanily <xanily@faldoria.de>
2018-06-27 22:24:30 +0200

    Update WCFSessionsAuthExtension

--- src/DependencyInjection/WCFSessionsAuthExtension.php
+++ src/DependencyInjection/WCFSessionsAuthExtension.php
@@ -6,6 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use xanily\WCFSessionsAuthBundle\Subscriber\AuthenticationSubscriber;
 
 /**
  * This is the class that loads and manages your bundle configuration.
@@ -22,12 +23,18 @@ class WCFSessionsAuthExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
+        $this->addAnnotatedClassesToCompile([
+            AuthenticationSubscriber::class,
+            ]);
+
+        $container->setParameter('wcf_sessions_auth.session.cookie_prefix', $config['session']['cookie_prefix']);
+        $container->setParameter('wcf_sessions_auth.session.login_page', $config['session']['login_page']);
+        $container->setParameter('wcf_sessions_auth.session.default_success_route', $config['session']['default_success_route']);
+        $container->setParameter('wcf_sessions_auth.database.entity_manager', $config['database']['entity_manager']);
+        $container->setParameter('wcf_sessions_auth.database.table_prefix', $config['database']['table_prefix']);
+        $container->setParameter('wcf_sessions_auth.roles', $config['roles']);
+
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
-
-        $container->setParameter('wcf_sessions_auth.session.cookie_prefix', $config['cookie_prefix']);
-        $container->setParameter('wcf_sessions_auth.session.default_success_route', $config['default_success_route']);
-        $container->setParameter('wcf_sessions_auth.database.table_prefix', $config['table_prefix']);
-
     }
 }
